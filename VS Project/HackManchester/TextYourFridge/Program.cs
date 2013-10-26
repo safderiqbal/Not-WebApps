01beea71f021ff465bd80dad7d71d03531c1ca93
Simon Levy <symi@users.noreply.github.com>
2013-10-26 15:59:26 +0100

    init commit of service to client sms

--- VS Project/HackManchester/TextYourFridge/Program.cs
+++ VS Project/HackManchester/TextYourFridge/Program.cs
@@ -1,6 +1,8 @@
-﻿using System;
+﻿using Clockwork;
+using System;
 using System.Collections.Generic;
 using System.Linq;
+using System.Net;
 using System.Text;
 using System.Threading.Tasks;
 
@@ -10,7 +12,44 @@ namespace TextYourFridge
     {
         static void Main(string[] args)
         {
-            
+            try
+            {
+                Clockwork.API api = new API("028646a5dff4200dd4539102cb07e37413de2896");
+                SMSResult result = api.Send(new SMS { To = "447763569935", Message = "Hello World" });
+
+                if (result.Success)
+                {
+                    Console.WriteLine("SMS Sent to {0}, Clockwork ID: {1}", result.SMS.To, result.ID);
+                }
+                else
+                {
+                    Console.WriteLine("SMS to {0} failed, Clockwork Error: {1} {2}", result.SMS.To, result.ErrorCode, result.ErrorMessage);
+                }
+
+                Console.ReadLine();
+            }
+            catch (APIException ex)
+            {
+                // You'll get an API exception for errors
+                // such as wrong username or password
+                Console.WriteLine("API Exception: " + ex.Message);
+            }
+            catch (WebException ex)
+            {
+                // Web exceptions mean you couldn't reach the Clockwork server
+                Console.WriteLine("Web Exception: " + ex.Message);
+            }
+            catch (ArgumentException ex)
+            {
+                // Argument exceptions are thrown for missing parameters,
+                // such as forgetting to set the username
+                Console.WriteLine("Argument Exception: " + ex.Message);
+            }
+            catch (Exception ex)
+            {
+                // Something else went wrong, the error message should help
+                Console.WriteLine("Unknown Exception: " + ex.Message);
+            }
         }
     }
 }
