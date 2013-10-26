eb01880b21b69ee0bfcdac32d9bb45c3e90b275f
nospamas <nospamas@localhost>
2013-10-26 20:42:01 +0100

    added ajax request for sending texts

--- NodeJs Project/app.js
+++ NodeJs Project/app.js
@@ -6,6 +6,7 @@
 var express = require('express');
 var routes = require('./routes');
 var user = require('./routes/user');
+var sendtext = require('./routes/sendtext');
 var http = require('http');
 var path = require('path');
 
@@ -30,6 +31,7 @@ if ('development' == app.get('env')) {
 
 app.get('/', routes.index);
 app.get('/users', user.list);
+app.post('/sendtext', sendtext.send)
 
 http.createServer(app).listen(app.get('port'), function(){
   console.log('Express server listening on port ' + app.get('port'));
---
+++ NodeJs Project/routes/sendtext.js
@@ -0,0 +1,9 @@
+
+/*
+ * POST send text.
+ */
+
+
+exports.send = function(req, res){
+    res.send("blerb");
+}
