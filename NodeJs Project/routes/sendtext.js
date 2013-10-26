69683a026c0d6b07c3e0b8f7b17f4995aabb4154
Simon Levy <Simon Levy@localhost>
2013-10-26 21:25:39 +0100

    fuck it works.

--- NodeJs Project/routes/sendtext.js
+++ NodeJs Project/routes/sendtext.js
@@ -2,8 +2,9 @@
 /*
  * POST send text.
  */
-
+var clockworkService = require('./clockworkService');
 
 exports.send = function(req, res){
+    clockworkService.send(req.body.to, req.body.text);
     res.send("blerb");
 }
