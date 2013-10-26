097d367683f6cabe87e0f3750b319a923060729a
nospamas <nospamas@localhost>
2013-10-26 18:43:46 +0100

    bootstrap and idea project files.

--- NodeJs Project/routes/index.js
+++ NodeJs Project/routes/index.js
@@ -4,5 +4,5 @@
  */
 
 exports.index = function(req, res){
-  res.render('index', { title: 'Express', sessionId: 'SESSIONID' });
+  res.render('index', { title: 'Text Routlette', sessionId: 'SESSIONID' });
 };
