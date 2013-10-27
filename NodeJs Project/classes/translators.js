5fd9120671e1976a2a8281dce6d102fe5b872993
Safder <safder-iqbal@users.noreply.github.com>
2013-10-27 11:35:40 +0000

    completed yandex

--- NodeJs Project/classes/translators.js
+++ NodeJs Project/classes/translators.js
@@ -143,12 +143,222 @@ exports['1337sp34k'] = function (message, callback) {
 
 exports.yandex = function (language, message, callback) {
     request.get(
-        '',
+        'https://translate.yandex.net/api/v1.5/tr.json/translate?key=' + yandex_key +
+            '&lang=' + language + '&text=' + message,
         {},
         function(error, response, body){
-            console.log('placeholder: ' + body);
+            console.log('yandex: ' + JSON.parse(body).text.join('').placeholder);
+            callback(JSON.parse(body).text.join(''));
         }
     )
+}
+
+exports['ru-en'] = function (message, callback) {
+    exports.yandex('ru-en',message,callback);
+}
+
+exports['en-ru'] = function (message, callback) {
+    exports.yandex('en-ru',message,callback);
+}
+
+exports['en-hu'] = function (message, callback) {
+    exports.yandex('en-hu',message,callback);
+}
+
+exports['en-uk'] = function (message, callback) {
+    exports.yandex('en-uk',message,callback);
+}
+
+exports['en-de'] = function (message, callback) {
+    exports.yandex('en-de',message,callback);
+}
+
+exports['en-fr'] = function (message, callback) {
+    exports.yandex('en-fr',message,callback);
+}
+
+exports['en-be'] = function (message, callback) {
+    exports.yandex('en-be',message,callback);
+}
+
+exports['en-fr'] = function (message, callback) {
+    exports.yandex('en-fr',message,callback);
+}
+
+exports['en-be'] = function (message, callback) {
+    exports.yandex('en-be',message,callback);
+}
+
+exports['en-sq'] = function (message, callback) {
+    exports.yandex('en-sq',message,callback);
+}
+
+exports['en-es'] = function (message, callback) {
+    exports.yandex('en-es',message,callback);
+}
+
+exports['en-it'] = function (message, callback) {
+    exports.yandex('en-it',message,callback);
+}
+
+exports['en-da'] = function (message, callback) {
+    exports.yandex('en-da',message,callback);
+}
+
+exports['en-pt'] = function (message, callback) {
+    exports.yandex('en-pt',message,callback);
+}
+
+exports['en-sk'] = function (message, callback) {
+    exports.yandex('en-sk',message,callback);
+}
+
+exports['en-sl'] = function (message, callback) {
+    exports.yandex('en-sl',message,callback);
+}
+
+exports['en-nl'] = function (message, callback) {
+    exports.yandex('en-nl',message,callback);
+}
+
+exports['en-ca'] = function (message, callback) {
+    exports.yandex('en-ca',message,callback);
+}
+
+exports['en-cs'] = function (message, callback) {
+    exports.yandex('en-cs',message,callback);
+}
+
+exports['en-el'] = function (message, callback) {
+    exports.yandex('en-el',message,callback);
+}
+
+exports['en-no'] = function (message, callback) {
+    exports.yandex('en-no',message,callback);
+}
+
+exports['en-mk'] = function (message, callback) {
+    exports.yandex('en-mk',message,callback);
+}
+
+exports['en-sv'] = function (message, callback) {
+    exports.yandex('en-sv',message,callback);
+}
+
+exports['en-fi'] = function (message, callback) {
+    exports.yandex('en-fi',message,callback);
+}
+
+exports['en-et'] = function (message, callback) {
+    exports.yandex('en-et',message,callback);
+}
+
+exports['en-lv'] = function (message, callback) {
+    exports.yandex('en-lv',message,callback);
+}
+
+exports['en-lt'] = function (message, callback) {
+    exports.yandex('en-lt',message,callback);
+}
+
+exports['en-tr'] = function (message, callback) {
+    exports.yandex('en-tr',message,callback);
+}
+
+exports['hu-en'] = function (message, callback) {
+    exports.yandex('hu-en',message,callback);
+}
+
+exports['uk-en'] = function (message, callback) {
+    exports.yandex('uk-en',message,callback);
+}
+
+exports['de-en'] = function (message, callback) {
+    exports.yandex('de-en',message,callback);
+}
+
+exports['fr-en'] = function (message, callback) {
+    exports.yandex('fr-en',message,callback);
+}
+
+exports['be-en'] = function (message, callback) {
+    exports.yandex('be-en',message,callback);
+}
+
+exports['sq-en'] = function (message, callback) {
+    exports.yandex('sq-en',message,callback);
+}
+
+exports['es-en'] = function (message, callback) {
+    exports.yandex('es-en',message,callback);
+}
+
+exports['it-en'] = function (message, callback) {
+    exports.yandex('it-en',message,callback);
+}
+
+exports['da-en'] = function (message, callback) {
+    exports.yandex('da-en',message,callback);
+}
+
+exports['pt-en'] = function (message, callback) {
+    exports.yandex('pt-en',message,callback);
+}
+
+exports['sk-en'] = function (message, callback) {
+    exports.yandex('sk-en',message,callback);
+}
+
+exports['sl-en'] = function (message, callback) {
+    exports.yandex('sl-en',message,callback);
+}
+
+exports['nl-en'] = function (message, callback) {
+    exports.yandex('nl-en',message,callback);
+}
+
+exports['ca-en'] = function (message, callback) {
+    exports.yandex('ca-en',message,callback);
+}
+
+exports['cs-en'] = function (message, callback) {
+    exports.yandex('cs-en',message,callback);
+}
+
+exports['el-en'] = function (message, callback) {
+    exports.yandex('el-en',message,callback);
+}
+
+exports['no-en'] = function (message, callback) {
+    exports.yandex('no-en',message,callback);
+}
+
+exports['mk-en'] = function (message, callback) {
+    exports.yandex('mk-en',message,callback);
+}
+
+exports['sv-en'] = function (message, callback) {
+    exports.yandex('sv-en',message,callback);
+}
+
+exports['fi-en'] = function (message, callback) {
+    exports.yandex('fi-en',message,callback);
+}
+
+exports['et-en'] = function (message, callback) {
+    exports.yandex('et-en',message,callback);
+}
+
+exports['lv-en'] = function (message, callback) {
+    exports.yandex('lv-en',message,callback);
+}
+
+exports['lt-en'] = function (message, callback) {
+    exports.yandex('lt-en',message,callback);
+}
+
+exports['tr-en'] = function (message, callback) {
+    exports.yandex('tr-en',message,callback);
 }
 
 //ERMAHGERD! HERPER METHERDS!
