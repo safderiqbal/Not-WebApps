df482a50d6f829166ba8d0c291c7382e2203658b
nospamas <nospamas@users.noreply.github.com>
2013-10-27 01:06:32 +0100

    prettified interface, added translation engine selection

--- NodeJs Project/routes/index.js
+++ NodeJs Project/routes/index.js
@@ -3,6 +3,8 @@
  * GET home page.
  */
 
+var available = require('../classes/translators').available;
+
 exports.index = function(req, res){
-    res.render('index', { title: 'Text Routlette', sessionId: 'SESSIONID' });
+  res.render('index', { title: 'Text Routlette', sessionId: 'SESSIONID', translators: available });
 };
